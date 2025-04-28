2be09182e5d123fe61a910074c12a6271b13e120
Travis <travisgerrard@gmail.com>
2025-04-28 05:08:38 -0700

    style: reduce bottom padding of chat area for less space below chats (Daddy Long Legs)

--- app/page.tsx
+++ app/page.tsx
@@ -315,7 +315,7 @@ export default function HomePage() {
       <div className="fixed inset-0 flex flex-col items-center justify-center bg-white dark:bg-gray-900">
         <div className="w-full max-w-5xl flex flex-col overflow-hidden h-full pt-16">
           {/* Chat Area - only scrollable region */}
-          <div className="flex-grow overflow-y-auto p-4 min-h-[300px] h-full pb-28">
+          <div className="flex-grow overflow-y-auto p-4 min-h-[300px] h-full pb-16">
             <ChatWindow messages={messages} isLoading={isWaitingForResponse} onRetryLastResponse={handleRetryLastResponse} />
           </div>
         </div>
